efc36f9f17f5e3ab0ac47e09837c1ca953ae6d10
DeltaRider <deltariderllc@gmail.com>
2019-04-06 10:31:22 -0700

    seeds file updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 /*
 	Make a seeds.sql file where you insert 5 actors into the actors table
 */
-USE actors_db;
+USE animals_db;
 
-INSERT INTO actors (actor_name) VALUES	("slyvester stalone");
-INSERT INTO actors (actor_name) VALUES	("ryan renold");
-INSERT INTO actors (actor_name) VALUES	("arnold shortsanager");
-INSERT INTO actors (actor_name) VALUES	("vin diesel");
-INSERT INTO actors (actor_name) VALUES	("selma high-eek");
+INSERT INTO animals (animal_name) VALUES	("dog");
+INSERT INTO animals (animal_name) VALUES	("cat");
+INSERT INTO animals (animal_name) VALUES	("bird");
+INSERT INTO animals (animal_name) VALUES	("goat");
+INSERT INTO animals (animal_name) VALUES	("monkey");
